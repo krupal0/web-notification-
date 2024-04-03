ea207ce82c981bd0952d4d9f4c188659e4c7a7d1
krupal.bhuva <krupal.bhuva@tatvasoft.com>
2024-04-03 15:59:12 +0530

    forward and reverse scrolling

--- lib/main.dart
+++ lib/main.dart
@@ -1,149 +1,116 @@
-import 'dart:async';
-import 'dart:html' as web;
-
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-import 'firebase_options.dart';
-
-main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  WidgetsFlutterBinding.ensureInitialized();
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Animation',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: PushNotificationApp(),
+      home: MyListView(),
     );
   }
 }
 
-/// Entry point for the example application.
-class PushNotificationApp extends StatefulWidget {
-  static const routeName = "/firebase-push";
-
+class MyListView extends StatefulWidget {
   @override
-  _PushNotificationAppState createState() => _PushNotificationAppState();
+  _MyListViewState createState() => _MyListViewState();
 }
 
-class _PushNotificationAppState extends State<PushNotificationApp> {
-  @override
-  void initState() {
-    getPermission();
-    messageListener(context);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire
-      future: Firebase.initializeApp(),
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          return Center(
-            child: Text(snapshot.error.toString()),
-          );
-        }
-        if (snapshot.connectionState == ConnectionState.done) {
-          return NotificationPage();
-        }
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      },
-    );
-  }
-
-  Future<void> getPermission() async {
-    FirebaseMessaging messaging = FirebaseMessaging.instance;
-
-    NotificationSettings settings = await messaging.requestPermission(
-      alert: true,
-      announcement: false,
-      badge: true,
-      carPlay: false,
-      criticalAlert: false,
-      provisional: false,
-      sound: true,
-    );
-
-    print('User granted permission: ${settings.authorizationStatus}');
-  }
-
-  void messageListener(BuildContext context) {
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {});
-  }
-}
-
-class NotificationPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _Application();
-}
-
-class _Application extends State<NotificationPage> {
-  late String _token;
-  int notificationCount = 0;
-
-  void setToken(String token) {
-    print('FCM TokenToken: $token');
-    setState(() {
-      _token = token;
-    });
-  }
+class _MyListViewState extends State<MyListView> {
+  ScrollController _scrollController = ScrollController();
+  List<int> _data = List.generate(20, (index) => index);
 
   @override
   void initState() {
     super.initState();
-    getToken();
+    _scrollController.addListener(_scrollListener);
   }
 
-  Future<void> getToken() async {
-    final fcmToken = await FirebaseMessaging.instance.getToken(
-        vapidKey:
-            "BHjIj9cxG9O1ZGwHt4FpOzQPuj6zZjvAFxqp602U_Eby3kcCEBpMy05Jm0QIuJsL5Zl3X9T8dfTyeIS4nZ8tths");
-    //get token
-    setToken(fcmToken!);
-    FirebaseMessaging.instance.getToken().then((value) => setToken(value!));
-    FirebaseMessaging.instance.onTokenRefresh.listen(setToken);
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollListener() {
+    // Check if scroll position is at the top
+    if (_scrollController.position.pixels == 0) {
+      List<int> newData = List.generate(10, (index) => index + _data.length);
+      setState(() {
+        _data.insertAll(0, newData);
+      });
+    }
+    if (_scrollController.offset >=
+        _scrollController.position.maxScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      List<int> newData = List.generate(10, (index) => index + _data.length);
+      setState(() {
+        _data.addAll(newData);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Firebase push notification'),
-        ),
-        body: Center(
-          child: Card(
-              margin: const EdgeInsets.all(10),
-              elevation: 10,
-              child: Center(
-                child: ElevatedButton(
-                  onPressed: () async {
-                    var permission = web.Notification.permission;
-                    if (permission != 'granted') {
-                      permission = await web.Notification.requestPermission();
-                    }
-                    if (permission == 'granted') {
-                      web.Notification( 'hello world ' );
-                    }
-                  },
-                  child: const Text('push notification'),
-                ),
-              )),
-        ));
+      appBar: AppBar(
+        title: const Text('ListView Example'),
+      ),
+      body: ListView.builder(
+        physics: const PositionRetainedScrollPhysics(),
+        controller: _scrollController,
+        itemCount: _data.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Item ${_data[index]}'),
+          );
+        },
+      ),
+    );
+  }
+}
+
+
+class PositionRetainedScrollPhysics extends ScrollPhysics {
+  final bool shouldRetain;
+  const PositionRetainedScrollPhysics({super.parent, this.shouldRetain = true});
+
+  @override
+  PositionRetainedScrollPhysics applyTo(ScrollPhysics? ancestor) {
+    return PositionRetainedScrollPhysics(
+      parent: buildParent(ancestor),
+      shouldRetain: shouldRetain,
+    );
+  }
+
+  @override
+  double adjustPositionForNewDimensions({
+    required ScrollMetrics oldPosition,
+    required ScrollMetrics newPosition,
+    required bool isScrolling,
+    required double velocity,
+  }) {
+    final position = super.adjustPositionForNewDimensions(
+      oldPosition: oldPosition,
+      newPosition: newPosition,
+      isScrolling: isScrolling,
+      velocity: velocity,
+    );
+
+    final diff = newPosition.maxScrollExtent - oldPosition.maxScrollExtent;
+    if (oldPosition.pixels > oldPosition.minScrollExtent &&
+        diff > 0 &&
+        shouldRetain) {
+
+      if(position > 0){
+        return position;
+      }else{
+        return position + diff;
+      }
+    } else {
+      return position;
+    }
   }
 }
